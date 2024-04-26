fdb3c8b37ae19f66f39ca2672592ffa508ebd84e
apatewen312 <apatewen@usc.edu>
2024-04-25 17:34:28 -0700

    Yummers Logo Changes
    - On profile page html: Changed "tl" class to "div-3"
    
    - Yummers logo class is now div-3
    
    - Added masterdoc Javascript

--- js/masterdoc.js
+++ js/masterdoc.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var yummersHeader = document.querySelector(".smallogo");
+  var yummersHeader = document.querySelector(".div-3");
 
   yummersHeader.addEventListener("click", function () {
     window.location.href = "http://127.0.0.1:5503/feed/feed.html";
