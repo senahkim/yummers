5fea1276f1bd1bb3cd360e2a4c81c9d4d5dcbfac
apatewen312 <apatewen@usc.edu>
2024-04-30 11:29:34 -0700

    Added Upload to Friends Camera

--- js/masterdoc.js
+++ js/masterdoc.js
@@ -5,3 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "http://127.0.0.1:5503/feed/feed.html";
   });
 });
+
+function toggleForm() {
+  var form = document.getElementById("camera-form");
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+  } else {
+    form.classList.add("hidden");
+  }
+}
